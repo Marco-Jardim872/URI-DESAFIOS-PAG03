04733a66593051d6007f3e9c21705d83fe1b095e
Marco Jardim <70395937+Marco-Jardim872@users.noreply.github.com>
2021-09-01 22:08:49 -0300

    Add files via upload

---
+++ 1143 - Quadrado e ao Cubo.js
@@ -0,0 +1,12 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var dados =input.split('\n');
+
+let num = parseInt(dados.shift());
+let arr=[0,0,0];
+
+for (let i = 0; i < num ; i++) {
+    arr[0]++;
+    arr[1]=arr[0]*arr[0];
+    arr[2]=arr[0]*arr[0]*arr[0];
+    console.log( arr.join(' '));
+}
